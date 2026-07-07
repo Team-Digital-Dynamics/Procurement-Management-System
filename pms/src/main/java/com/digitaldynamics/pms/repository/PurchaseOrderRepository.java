d159a3007107df9f207c5c8b83c1bb12481fb4e0
Kayla Locke <kaylalocke2001@gmail.com>
2026-07-07 23:51:14 +0200

    PurchaseOrderUpdate
    Updated PurchaseOrderRepository.java
    Create PurchaseOrderDtos.java
    Create PurchaseOrderService.java

--- pms/src/main/java/com/digitaldynamics/pms/repository/PurchaseOrderRepository.java
+++ pms/src/main/java/com/digitaldynamics/pms/repository/PurchaseOrderRepository.java
@@ -1,9 +1,15 @@
 package com.digitaldynamics.pms.repository;
 
 import com.digitaldynamics.pms.model.PurchaseOrder;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+
+    @EntityGraph(attributePaths = { "quotation", "supplier" })
+    List<PurchaseOrder> findAllByOrderByCreatedAtDesc();
+
     Optional<PurchaseOrder> findByQuotationId(Long quotationId);
 }
